3b637cd6d7acdc4dabd3599d6b5be20b7124a542
abdurisaq <ahh8@sfu.ca>
2023-11-20 22:48:07 -0800

    added tab buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import HomePage from './pages/home';
 import AboutPage from './pages/about';
 import React, { useState } from 'react';
-
+import Sidebar from './sidebar';
 
 function App() {
   const [page, setPage] = useState('home');
@@ -11,7 +11,7 @@ function App() {
   const goToAboutMe = () => setPage('aboutMe');
   return (
     <div className="App">
-      
+      <Sidebar setPage={setPage} />
       {page === 'home' && <HomePage goToAboutMe={goToAboutMe} />}
       {page === 'aboutMe' && <AboutPage />}
       
