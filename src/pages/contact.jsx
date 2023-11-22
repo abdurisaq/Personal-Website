08dccf513f8074d717a61da7b4fa8fed2e7ab9f8
abdurisaq <ahh8@sfu.ca>
2023-11-21 21:49:46 -0800

    added contact information, issue 8

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,18 +3,17 @@ import React from 'react';
 function Contact() {
   const contactInfo = {
     phoneNumber: '123-456-7890',
-    email: 'example@example.com',
+    email: 'mailto:abdurizaq@outlook.com',
     twitter: 'https://twitter.com/Dur1RL',
-    linkedIn: 'https://linkedin.com/in/example'
+    
   };
 
   return (
     <div>
       <h1>Contact Me</h1>
       <p>Phone Number: {contactInfo.phoneNumber}</p>
-      <p>Email: {contactInfo.email}</p>
-      <p>Twitter: <a href={contactInfo.twitter}>@example</a></p>
-      <p>LinkedIn: <a href={contactInfo.linkedIn}>example</a></p>
+      <p>Email: <a href ={contactInfo.email}>abdurizaq@outlook.com</a></p>
+      <p>Twitter: <a href={contactInfo.twitter}>DuriRL</a></p>
     </div>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,11 +11,11 @@ function Home({ goToAboutMe }) {
       </section>
       <section>
         <h2>Computer Science Student</h2>
-        <p>bio</p>
+        <p>Hey! I'm a second-year student at Simon Fraser University, deeply invested in the captivating worlds of game design and machine learning. Juggling frontend and backend development, I'm honing my skills in these domains. Actively seeking co-op opportunities, I'm eager to expand my horizons and dive deeper into the realms of technology, particularly in game design, machine learning, and software development.</p>
       </section>
       <button onClick={goToAboutMe}>About Me</button>
 
-     <div>Image of me Here</div>
+     
     </div>
   );
 }
